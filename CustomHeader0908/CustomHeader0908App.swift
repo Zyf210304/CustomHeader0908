8b1b6fc150946b723aaf37d71adc311ca833b5be
张亚飞 <672233580@qq.com>
2021-09-08 09:52:54 +0800

    Initial Commit

---
+++ CustomHeader0908/CustomHeader0908App.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHeader0908App.swift
+//  CustomHeader0908
+//
+//  Created by 张亚飞 on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct CustomHeader0908App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
